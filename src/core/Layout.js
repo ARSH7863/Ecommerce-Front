50fca7d41a300341313320015062846802ef4269
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2021-08-26 13:21:37 +1100

    Carousel & tooltip added

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -1,24 +1,68 @@
 import React from "react";
 import Menu from "./Menu";
+import Carousel from "react-bootstrap/Carousel";
+import carsouselPhoto1 from "../assets/img/Brand-image-flute-2-3.jpg";
+import carsouselPhoto2 from "../assets/img/nepsis-3.jpeg";
+import carsouselPhoto3 from "../assets/img/vasmol-3.jpg";
 import "../styles.css";
-
 const Layout = ({
-  title = "Title",
-  descripton = "Description",
-  className,
-  children,
+	title = "Title",
+	descripton = "Description",
+	className,
+	children,
 }) => {
-  return (
-    <div>
-      <Menu />
-      <div className="jumbotron text-center">
+	return (
+		<div>
+			<Menu />
+			{/* <div className="jumbotron text-center">
         <br />
         <h2>{title}</h2>
         <p className="lead"> {descripton} </p>
-      </div>
-      <div className={className}>{children}</div>
-    </div>
-  );
+      </div> */}
+			<div style={{ display: "block", paddingTop: 30, paddingBottom: 30 }}>
+				<Carousel fade>
+					<Carousel.Item interval={1500}>
+						<img
+							className="d-block w-100"
+							src={carsouselPhoto1}
+							alt="First slide"
+						/>
+						{/* <Carousel.Caption>
+						<h3>First slide label</h3>
+						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</Carousel.Caption> */}
+					</Carousel.Item>
+					<Carousel.Item interval={1500}>
+						<img
+							className="d-block w-100"
+							src={carsouselPhoto2}
+							alt="Second slide"
+						/>
+
+						{/* <Carousel.Caption>
+						<h3>Second slide label</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</Carousel.Caption> */}
+					</Carousel.Item>
+					<Carousel.Item interval={1500}>
+						<img
+							className="d-block w-100"
+							src={carsouselPhoto3}
+							alt="Third slide"
+						/>
+
+						{/* <Carousel.Caption>
+						<h3>Third slide label</h3>
+						<p>
+							Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+						</p>
+					</Carousel.Caption> */}
+					</Carousel.Item>
+				</Carousel>
+			</div>
+			<div className={className}>{children}</div>
+		</div>
+	);
 };
 
 export default Layout;
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -4,117 +4,117 @@ import Layout from "../core/Layout";
 import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
-  const [values, setValues] = useState({
-    email: "abc@gmail.com",
-    password: "abc7863",
-    error: "",
-    loading: false,
-    redirectToReferrer: false,
-  });
+	const [values, setValues] = useState({
+		email: "abc@gmail.com",
+		password: "abc7863",
+		error: "",
+		loading: false,
+		redirectToReferrer: false,
+	});
 
-  const { email, password, loading, error, redirectToReferrer } = values;
-  const { user } = isAuthenticated();
+	const { email, password, loading, error, redirectToReferrer } = values;
+	const { user } = isAuthenticated();
 
-  const handleChange = (name) => (event) => {
-    setValues({ ...values, error: false, [name]: event.target.value });
-  };
+	const handleChange = (name) => (event) => {
+		setValues({ ...values, error: false, [name]: event.target.value });
+	};
 
-  const clickSubmit = (event) => {
-    // console.log(name,email,password);
-    event.preventDefault();
-    setValues({ ...values, error: false, loading: true });
-    signin({ email, password }).then((data) => {
-      if (data.error) {
-        setValues({ ...values, error: data.error, loading: false });
-      } else {
-        authenticate(data, () => {
-          setValues({
-            ...values,
-            redirectToReferrer: true,
-          });
-        });
-      }
-    });
-  };
+	const clickSubmit = (event) => {
+		// console.log(name,email,password);
+		event.preventDefault();
+		setValues({ ...values, error: false, loading: true });
+		signin({ email, password }).then((data) => {
+			if (data.error) {
+				setValues({ ...values, error: data.error, loading: false });
+			} else {
+				authenticate(data, () => {
+					setValues({
+						...values,
+						redirectToReferrer: true,
+					});
+				});
+			}
+		});
+	};
 
-  const signInForm = () => (
-    <div className="row p-2">
-      <div className="col-sm-12 ml-4">
-        <form>
-          <div className="form-group">
-            <label className="text-muted">Email</label>
-            <input
-              onChange={handleChange("email")}
-              type="email"
-              className="form-control"
-              value={email}
-            />
-          </div>
+	const signInForm = () => (
+		<div className="row p-2">
+			<div className="col-sm-12 ml-4">
+				<form>
+					<div className="form-group">
+						<label className="text-muted">Email</label>
+						<input
+							onChange={handleChange("email")}
+							type="email"
+							className="form-control"
+							value={email}
+						/>
+					</div>
 
-          <div className="form-group">
-            <label className="text-muted">Password</label>
-            <input
-              onChange={handleChange("password")}
-              type="password"
-              className="form-control"
-              value={password}
-            />
-          </div>
-          <div className="row">
-            <div className="col-5"></div>
-            <div className="col-5">
-              <button onClick={clickSubmit} className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-            <div className="col-2"></div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+					<div className="form-group">
+						<label className="text-muted">Password</label>
+						<input
+							onChange={handleChange("password")}
+							type="password"
+							className="form-control"
+							value={password}
+						/>
+					</div>
+					<div className="row">
+						<div className="col-5"></div>
+						<div className="col-5">
+							<button onClick={clickSubmit} className="btn btn-primary">
+								Submit
+							</button>
+						</div>
+						<div className="col-2"></div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 
-  const showError = () => (
-    <div
-      className="alert alert-danger"
-      style={{ display: error ? "" : "none" }}
-    >
-      {error}
-    </div>
-  );
+	const showError = () => (
+		<div
+			className="alert alert-danger"
+			style={{ display: error ? "" : "none" }}
+		>
+			{error}
+		</div>
+	);
 
-  const showLoading = () =>
-    loading && (
-      <div className="alert alert-info">
-        <h2>Loading...</h2>
-      </div>
-    );
+	const showLoading = () =>
+		loading && (
+			<div className="alert alert-info">
+				<h2>Loading...</h2>
+			</div>
+		);
 
-  const redirectUser = () => {
-    if (redirectToReferrer) {
-      if (user && user.role === 1) {
-        return <Redirect to="/admin/dashboard" />;
-      } else {
-        return <Redirect to="/user/dashboard" />;
-      }
-    }
-    if (isAuthenticated()) {
-      return <Redirect to="/" />;
-    }
-  };
+	const redirectUser = () => {
+		if (redirectToReferrer) {
+			if (user && user.role === 1) {
+				return <Redirect to="/admin/dashboard" />;
+			} else {
+				return <Redirect to="/user/dashboard" />;
+			}
+		}
+		if (isAuthenticated()) {
+			return <Redirect to="/" />;
+		}
+	};
 
-  return (
-    <Layout
-      title="Signin"
-      descripton={`Please SignIn to Continue.`}
-      className="container col-md-8 offset-md-2"
-    >
-      {showLoading()}
-      {showError()}
-      {signInForm()}
-      {redirectUser()}
-    </Layout>
-  );
+	return (
+		<Layout
+			title="Signin"
+			descripton={`Please SignIn to Continue.`}
+			className="container col-md-8 offset-md-2"
+		>
+			{showLoading()}
+			{showError()}
+			{signInForm()}
+			{redirectUser()}
+		</Layout>
+	);
 };
 
 export default Signin;
